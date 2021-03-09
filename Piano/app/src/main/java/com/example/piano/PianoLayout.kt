55460dca27fbb99199102b23567e02adf550b688
Kristian Utvær <kristianu@uia.no>
2021-03-09 13:34:27 +0100

    la til lagre funksjon for appen, fikk ikke til unit testing og ui kunne sett finere ut

--- Piano/app/src/main/java/com/example/piano/PianoLayout.kt
+++ Piano/app/src/main/java/com/example/piano/PianoLayout.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.piano.data.Note
 import com.example.piano.databinding.FragmentPianoBinding
 import kotlinx.android.synthetic.main.fragment_piano.view.*
+import java.io.File
+import java.io.FileOutputStream
 
 class PianoLayout : Fragment() {
 
@@ -16,7 +20,7 @@ class PianoLayout : Fragment() {
     private val fullNoter = listOf("C","D","E","F","G","A","B","C2","D2","E2","F2","G2")
 
     private val halvNoter = listOf("C#","D#","E#","F#","G#","A#","B#")
-
+    private var noteListe:MutableList<Note> = mutableListOf<Note>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,13 +38,23 @@ class PianoLayout : Fragment() {
 
         fullNoter.forEach {
             val fullNoterPianoKey = FullNoterPianoKeyFrag.newInstance(it)
+            var startPlay:Long = 0
 
-            fullNoterPianoKey.onKeyDown = {
-                println("Piano knapp er nede $it")
+            fullNoterPianoKey.onKeyDown = { note ->
+                startPlay = System.nanoTime()
+                println("Piano knapp er nede $note")
             }
 
             fullNoterPianoKey.onKeyUp = {
-                println("Piano key op $it")
+                var endPlay = System.nanoTime()
+                var fullTonerTotTid: Double = 0.0
+                var fullTonerTid: Long = 0
+                fullTonerTid = endPlay - startPlay
+                fullTonerTotTid = fullTonerTid.toDouble() / 1000000000
+
+                val note = Note(it, startPlay, fullTonerTotTid)
+                noteListe.add(note)
+                println("Piano key op $note")
             }
 
             ft.add(view.fullNoteKeyLayout.id, fullNoterPianoKey, "note_$it")
@@ -60,6 +74,40 @@ class PianoLayout : Fragment() {
         }
 
         ft.commit()
+
+
+        view.saveScoreBt.setOnClickListener {
+            var fileNavn = view.filNavnTextEdit.text.toString()
+            val path = this.activity?.getExternalFilesDir(null)
+            val nyNoteFil = (File(path, fileNavn))
+
+            when {
+                noteListe.count() == 0 -> Toast.makeText(activity, "Forgot notes? Did you click by mistake?", Toast.LENGTH_SHORT).show()
+                fileNavn.isEmpty() -> Toast.makeText(activity, "Forgot filename?", Toast.LENGTH_SHORT).show()
+                path == null -> Toast.makeText(activity, "Are you sure this is the right path?", Toast.LENGTH_SHORT).show()
+                nyNoteFil.exists() -> Toast.makeText(activity, "You already did this one!", Toast.LENGTH_SHORT).show()
+
+
+                else -> {
+                    fileNavn = "$fileNavn.txt"
+                    FileOutputStream(nyNoteFil, true).bufferedWriter().use { writer ->
+                        noteListe.forEach {
+                            writer.write("${it.toString()}\n")
+                        }
+                        FileOutputStream(nyNoteFil).close()
+                    }
+
+                    Toast.makeText(activity, "Great job, Beethoven! Your file was successful.", Toast.LENGTH_SHORT).show()
+                    noteListe.clear()
+
+
+
+
+                    println("File saves as $fileNavn at $path/$fileNavn")
+                }
+
+            }
+        }
 
         return view
     }
